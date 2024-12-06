9ac9a1b9dacc4c9bedd5ccaf1b751f383a4bed5b
Ksenia Sevryukova <ksu0985@mail.ru>
2024-12-06 16:41:44 +0300

    new

---
+++ src/test/java/selenideRepositorySearchtest.java
@@ -0,0 +1,47 @@
+package github;
+
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class selenideRepositorySearchtest {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://github.com";
+        Configuration.pageLoadStrategy = "eager";
+    }
+    @Test
+    void shouldFindSelenideRepositoryAtTheTop() {
+        // открыть страницу Selenide в Github
+        open("/selenide/selenide");
+        // подвести мышку к wiki и кликнуть
+        $("#wiki-tab").click();
+        $("#wiki-pages-filter").setValue("SoftAssertions");
+        $(".filterable-active").shouldHave(text("SoftAssertions"));
+        $(".filterable-active").$(byText("SoftAssertions")).click();
+        $("#wiki-body").shouldHave(text("@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}"));
+    }
+    @AfterEach
+    void afterEachteardown() {
+        Selenide.closeWebDriver();
+    }
+}
